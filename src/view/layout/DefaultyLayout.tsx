5a9730a9617946e50bbee96a270c61a06ab61327
João Portela <136658564+Portela03@users.noreply.github.com>
2024-09-17 04:25:04 +0000

    feat(header): criando o Header

--- src/view/layout/DefaultyLayout.tsx
+++ src/view/layout/DefaultyLayout.tsx
@@ -1,15 +1,18 @@
 import { Outlet } from 'react-router-dom';
+import { Header } from '../screens/Header/Header';
 import { Footer } from './footer/footer';
+
 export function DefaultLayout() {
   return (
     <div>
-      <Footer />
+      <Header />
       <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
         <main>
           <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
             <Outlet />
           </div>
         </main>
+        <Footer />
       </div>
     </div>
   );
