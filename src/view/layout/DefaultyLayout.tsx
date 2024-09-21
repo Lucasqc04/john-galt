c4d6b4d60eb41d4721d81dc34893c2cd47ad8407
João Portela <136658564+Portela03@users.noreply.github.com>
2024-09-21 04:10:49 +0000

    feat(footer): crete new footer

--- src/view/layout/DefaultyLayout.tsx
+++ src/view/layout/DefaultyLayout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import { Footer } from './Footer';
 import Header from './Header';
 
 export function DefaultLayout() {
   return (
-    <div className="text-black dark:text-white w-full max-w-[100vw]">
+    <div className="text-black dark:text-white w-full max-w-[100vw] m-0">
       <Header />
       <main>
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/view/layout/Footer.tsx
@@ -0,0 +1,48 @@
+export function Footer() {
+  return (
+    <footer className="bg-white rounded-t-xl shadow dark:bg-gray-800">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <a
+            href="#"
+            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
+            <h1 className="font-extrabold self-center text-2xl whitespace-nowrap dark:text-white-white">
+              DIY LAB
+            </h1>
+          </a>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium dark:text-white-white sm:mb-0 dark:text-white-gray-400">
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Licensing
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:text-white-white lg:my-8" />
+        <span className="block text-smdark:text-white-white sm:text-center dark:text-white-gray-400">
+          © 2024{' '}
+          <a href="#" className="hover:underline">
+            DIY LAB™
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
+}
