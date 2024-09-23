47d88d2a6aefd9708c18899b53138ff02e30f03d
jefflds <jeffersonlds3009@gmail.com>
2024-09-22 21:13:34 -0300

    refactor(blog-links): change some styles

--- src/view/screens/partials/BlogLinks.tsx
+++ src/view/screens/partials/BlogLinks.tsx
@@ -22,7 +22,7 @@ export function BlogLinks() {
         <h1 className="text-center text-3xl lg:text-5xl tracking-wider text-gray-900 dark:text-white">
           {t('BlogTitle')}
         </h1>
-        <div className="mt-12 lg:mt-24">
+        <div className="pt-12 lg:pt-24">
           <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-8">
             <div>
               <img
@@ -45,11 +45,11 @@ export function BlogLinks() {
                 <p className="text-gray-700 dark:text-gray-300 text-base lg:text-lg lg:leading-8 tracking-wide mt-6 w-11/12">
                   {t('CardDescription1')}
                 </p>
-                <div className="w-full mt-4 justify-end flex items-center cursor-pointer">
+                <div className="w-full pt-4 justify-end flex items-center cursor-pointer gap-x-3 lg:gap-x-6">
                   <p className="text-base tracking-wide text-orange-500">
                     {t('ReadMore')}
                   </p>
-                  <MdArrowForward size={28} className="ml-3 lg:ml-6" />
+                  <MdArrowForward size={28} />
                 </div>
               </div>
             </div>
@@ -102,7 +102,7 @@ export function BlogLinks() {
                   </div>
                 </div>
               </div>
-              <div className="mt-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8">
+              <div className="pt-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8">
                 <div>
                   <img
                     className="w-full"
