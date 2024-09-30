d144063b3eb40497f273b22584a51b15b90521b7
jefflds <jeffersonlds3009@gmail.com>
2024-09-30 12:01:38 +0000

    feat(vite.config): add real time changes in host

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      clientPort: 5173,
+    },
   },
 });
