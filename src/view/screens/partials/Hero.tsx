089485a8af7fa8e55b7e3a45cc1feb2d5d0164cb
João Portela <136658564+Portela03@users.noreply.github.com>
2024-10-10 01:50:32 +0000

    style(hero): add padding-X to mobile screens

--- src/view/screens/partials/Hero.tsx
+++ src/view/screens/partials/Hero.tsx
@@ -42,7 +42,7 @@ export function Hero() {
       <BackgroundAnimated />
       <section
         ref={ref}
-        className="grid grid-cols-1 md:grid-cols-12 px-4 md:px-8 h-screen"
+        className="grid grid-cols-1 md:grid-cols-12 px-6 sm:px-4 md:px-8 h-screen"
       >
         <article className="h-full col-span-12 md:col-span-6 flex flex-col gap-y-4 items-center justify-center">
           <h1 className="max-w-2xl text-4xl md:text-6xl text-center font-bold whitespace-pre-wrap break-words animate-fade-right animate-once animate-duration-500 animate-delay-300">
