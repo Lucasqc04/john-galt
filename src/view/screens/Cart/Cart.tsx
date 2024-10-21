53d6cdd2170a35b82358394cb368e33280870386
jefflds <jeffersonlds3009@gmail.com>
2024-10-21 13:55:30 +0000

    feat(cart): increment styles

--- src/view/screens/Cart/Cart.tsx
+++ src/view/screens/Cart/Cart.tsx
@@ -1,11 +1,13 @@
 import { FaTrash } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { IoArrowBack } from 'react-icons/io5';
+import { Link, useNavigate } from 'react-router-dom';
 import { LanguageTexts } from '../../../domain/locales/Language';
 import { ROUTES } from '../../routes/Routes';
 import { useCart } from './useCart';
 
 export function Cart() {
   const { t, currentLang, cart } = useCart();
+  const navigate = useNavigate();
 
   if (cart.items.length === 0) {
     return (
@@ -20,39 +22,49 @@ export function Cart() {
   return (
     <div className="min-h-screen pt-[15%] md:pt-[10%] lg:pt-[5%] dark:bg-gray-900">
       <div className="max-w-7xl mx-auto p-4">
-        <h2 className="text-3xl font-bold mb-4 dark:text-white">
-          {t(LanguageTexts.cart.title)}
-        </h2>
-        <ul>
-          {cart.items.map((item) => (
-            <li
-              key={item.id}
-              className="mb-4 flex items-center border-b border-gray-300 pb-4 dark:border-gray-700"
-            >
-              <img
-                src={item.imageUrl}
-                alt={item.name}
-                className="w-16 h-16 mr-4"
-              />
-              <div className="flex-1">
-                <p className="font-semibold dark:text-white">{item.name}</p>
-                <p className="dark:text-gray-300">
-                  {t(LanguageTexts.cart.price)}: ${item.price.toFixed(2)}
-                </p>
-                <p className="dark:text-gray-300">
-                  {t(LanguageTexts.cart.quantity)}: {item.quantity}
-                </p>
-              </div>
-              <button
-                onClick={() => cart.remove(item.id)}
-                className="bg-red-500 text-white px-4 py-2 rounded ml-4 flex items-center"
+        <div className="flex items-center gap-x-4 border-b pb-2">
+          <button onClick={() => navigate(-1)}>
+            <IoArrowBack size={24} />
+          </button>
+          <h2 className="text-3xl font-bold dark:text-white">
+            {t(LanguageTexts.cart.title)}
+          </h2>
+        </div>
+        <div className="w-full pt-4 flex flex-col gap-y-6">
+          <p className="font-semibold">
+            Você tem {cart.items.length} items no carrinho
+          </p>
+          <ul>
+            {cart.items.map((item) => (
+              <li
+                key={item.id}
+                className="mb-4 flex items-center border-b border-gray-300 pb-4 dark:border-gray-700"
               >
-                <FaTrash className="mr-2" />
-                {t(LanguageTexts.cart.remove)}
-              </button>
-            </li>
-          ))}
-        </ul>
+                <img
+                  src={item.imageUrl}
+                  alt={item.name}
+                  className="w-16 h-16 mr-4"
+                />
+                <div className="flex-1">
+                  <p className="font-semibold dark:text-white">{item.name}</p>
+                  <p className="dark:text-gray-300">
+                    {t(LanguageTexts.cart.price)}: ${item.price.toFixed(2)}
+                  </p>
+                  <p className="dark:text-gray-300">
+                    {t(LanguageTexts.cart.quantity)}: {item.quantity}
+                  </p>
+                </div>
+                <button
+                  onClick={() => cart.remove(item.id)}
+                  className="bg-red-500 text-white px-4 py-2 rounded ml-4 flex items-center"
+                >
+                  <FaTrash className="mr-2" />
+                  {t(LanguageTexts.cart.remove)}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
         <div className="flex justify-between">
           <div className="flex flex-col gap-y-2">
             <button
