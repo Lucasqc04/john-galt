a8f87ccd4c4f56204bb2f91061bedb9e81927e99
jefflds <jeffersonlds3009@gmail.com>
2024-10-21 12:03:39 +0000

    refactor(notfound): simplify code

--- src/view/screens/NotFound.tsx
+++ src/view/screens/NotFound.tsx
@@ -1,14 +1,8 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Logo from '../assets/logo.svg';
 import { ROUTES } from '../routes/Routes';
 
 export function NotFound() {
-  const navigate = useNavigate();
-
-  const handleHomeRedirect = () => {
-    navigate(ROUTES.home.call());
-  };
-
   return (
     <div className="min-h-screen bg-primary-light dark:bg-primary-dark text-black dark:text-white flex flex-col items-center justify-center p-4">
       <img src={Logo} alt="Logo da Empresa" className="mb-4 w-24 h-24" />
@@ -16,12 +10,12 @@ export function NotFound() {
       <p className="text-lg mb-6">
         It seems like you’ve tried to access a page that doesn’t exist.
       </p>
-      <button
-        onClick={handleHomeRedirect}
+      <Link
+        to={ROUTES.home.call()}
         className="px-6 py-3 bg-[#F6911D] text-white rounded-lg transition"
       >
         Back to DIY LAB
-      </button>
+      </Link>
     </div>
   );
 }
