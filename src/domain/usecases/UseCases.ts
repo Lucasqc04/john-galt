f7dbc34121b85a4e9132ef50c61303eda58669b6
jefflds <jeffersonlds3009@gmail.com>
2024-10-24 20:04:51 -0300

    feat(coupon): all data/domain flow

--- src/domain/usecases/UseCases.ts
+++ src/domain/usecases/UseCases.ts
@@ -1,7 +1,9 @@
 import { RemoteDataSource } from '../../data/datasource/Remote.datasource';
+import { CouponRepositoryImpl } from '../../data/repositories/Coupon.repository';
 import { NewsletterRepositoryImpl } from '../../data/repositories/Newsletter.repository';
 import { PaymentRepositoryImpl } from '../../data/repositories/Payment.repository';
 import { ShippingRepositoryImpl } from '../../data/repositories/Shipping,repository';
+import { ValidateCouponUseCaseImpl } from './Coupons/validate.usecase';
 import { InsertNewsletterUseCaseImpl } from './Newsletter/insert.usecase';
 import { CreatePaymentUseCaseImpl } from './Payment/Create.usecase';
 import { CalculateShippingUseCaseImpl } from './Shipping/Calculate.usecase';
@@ -13,6 +15,7 @@ const api = new RemoteDataSource(API_URL);
 const NewsletterRepository = new NewsletterRepositoryImpl(api);
 const ShippingRepository = new ShippingRepositoryImpl(api);
 const PaymentRepository = new PaymentRepositoryImpl(api);
+const CouponRepository = new CouponRepositoryImpl(api);
 
 export const UseCases = {
   newsletter: {
@@ -23,5 +26,8 @@ export const UseCases = {
   },
   payment: {
     create: new CreatePaymentUseCaseImpl(PaymentRepository),
+  },
+  coupon: {
+    validate: new ValidateCouponUseCaseImpl(CouponRepository),
   },
 };
