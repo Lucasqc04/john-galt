346ee3350343212745f4c6ba8ceb9e6ab268ac85
jefflds <jeffersonlds3009@gmail.com>
2024-11-20 12:43:54 -0300

    feat(cart): improve styles in dark mode

--- src/view/screens/Cart/Cart.tsx
+++ src/view/screens/Cart/Cart.tsx
@@ -14,7 +14,7 @@ export function Cart() {
 
   if (cart.items.length === 0) {
     return (
-      <div className="min-h-screen flex items-center justify-center dark:bg-gray-900">
+      <div className="min-h-screen flex items-center justify-center dark:bg-primary-dark">
         <h1 className="text-4xl font-bold dark:text-white">
           {t(LanguageTexts.cart.emptyCart)}
         </h1>
@@ -23,7 +23,7 @@ export function Cart() {
   }
 
   return (
-    <div className="min-h-screen pt-20 sm:pt-24 dark:bg-gray-900">
+    <div className="min-h-screen pt-20 sm:pt-24 dark:bg-primary-dark">
       <div className="max-w-7xl mx-auto p-4">
         <div className="flex items-center gap-x-4 border-b pb-2">
           <button onClick={() => navigate(-1)}>
@@ -96,7 +96,7 @@ export function Cart() {
           <div className="flex flex-col gap-y-2">
             <button
               onClick={cart.clear}
-              className="bg-[#F6911D] text-white px-4 py-2 rounded"
+              className="bg-orange-primary text-white px-4 py-2 rounded"
             >
               {t(LanguageTexts.cart.clearCart)}
             </button>
