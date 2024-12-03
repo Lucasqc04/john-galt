107662437d6bb712b03bd57e374107218ac8a82e
João Portela <136658564+Portela03@users.noreply.github.com>
2024-12-03 16:48:49 +0000

    fix: cahnge the mensagem for whatsapp

--- src/view/components/buttonWhatsApp.tsx
+++ src/view/components/buttonWhatsApp.tsx
@@ -5,7 +5,8 @@ function WhatsAppButton() {
     <button
       onClick={() =>
         window.open(
-          'https://api.whatsapp.com/send?phone=+5511919050416&text=Ol%C3%A1,%20Tudo%20bem?%0A%0AEu%20gostaria%20de%20saber%20mais%20sobre%20os%20produtos%20da%20DIY%20LAB...',
+          'https://api.whatsapp.com/send?phone=+5511919050416&text=Ol%C3%A1,%20Tudo%20bem?%0A%0APreciso%20de%20ajuda%20sobre%20os%20produtos%20da%20DIY%20LAB...',
+
           '_blank',
         )
       }
