7397263704a3d85f99d09e61612a418bde00e55f
Portela03 <joao.portela03@gmail.com>
2024-12-26 19:53:33 +0000

    feat: change routes for alfred

--- src/view/routes/BrowserRouter.tsx
+++ src/view/routes/BrowserRouter.tsx
@@ -8,7 +8,6 @@ import {
 import { AcceptedLanguages, useLanguage } from '../../domain/locales/Language';
 import { Loader } from '../components/Loader';
 import ScrollToTop from '../components/ScrollToTop';
-import { DefaultLayout } from '../layout/DefaultyLayout';
 import { AboutBuyBitcoin } from '../screens/BuyBitcoin/AboutBuyBitcoin';
 import BuyBitcoin from '../screens/BuyBitcoin/BuyBitcoin';
 import BuyCheckout from '../screens/BuyBitcoin/BuyCheckout';
@@ -16,25 +15,8 @@ import { PaymentAlfredFailure } from '../screens/BuyBitcoin/pageFailure';
 import { PaymentAlfredSuccess } from '../screens/BuyBitcoin/pageSucces';
 import { Fees } from '../screens/BuyBitcoin/RateBitcoin';
 import { TermsOfUse } from '../screens/BuyBitcoin/TermsUse';
-import { BitcoinPayment } from '../screens/Checkout/BitcoinPayment';
-import { PixPayment } from '../screens/Checkout/PixPayment';
 import { ROUTES } from './Routes';
 
-const About = lazy(() =>
-  import('../screens/About').then((module) => ({
-    default: module.About,
-  })),
-);
-const BlogPost = lazy(() =>
-  import('../screens/BlogPost/BlogPost').then((module) => ({
-    default: module.BlogPost,
-  })),
-);
-const Cart = lazy(() =>
-  import('../screens/Cart/Cart').then((module) => ({
-    default: module.Cart,
-  })),
-);
 const Checkout = lazy(() =>
   import('../screens/Checkout/Checkout').then((module) => ({
     default: module.Checkout,
@@ -50,44 +32,9 @@ const Page = lazy(() =>
     default: module.Page,
   })),
 );
-const Products = lazy(() =>
-  import('../screens/partials/Products').then((module) => ({
-    default: module.Products,
-  })),
-);
-const PaymentFailure = lazy(() =>
-  import('../screens/paymentStatus/PaymentFailure').then((module) => ({
-    default: module.PaymentFailure,
-  })),
-);
-const PaymentSuccess = lazy(() =>
-  import('../screens/paymentStatus/PaymentSucess').then((module) => ({
-    default: module.PaymentSuccess,
-  })),
-);
-const PrivacyPolicy = lazy(() =>
-  import('../screens/PrivacyPolicy').then((module) => ({
-    default: module.PrivacyPolicy,
-  })),
-);
-const ProductPage = lazy(() =>
-  import('../screens/ProductPage/ProductPage').then((module) => ({
-    default: module.ProductPage,
-  })),
-);
 const SupportPage = lazy(() =>
   import('../screens/support').then((module) => ({
     default: module.Support,
-  })),
-);
-const TutorialsPage = lazy(() =>
-  import('../screens/TutorialsPage').then((module) => ({
-    default: module.TutorialsPage,
-  })),
-);
-const VideosPage = lazy(() =>
-  import('../screens/Videos').then((module) => ({
-    default: module.VideosPage,
   })),
 );
 
@@ -105,34 +52,15 @@ export function BrowserRouter() {
               <Navigate to={`/${currentLang || AcceptedLanguages.pt}`} />
             }
           />
-          <Route path={ROUTES.lang.call()} element={<DefaultLayout />}>
+          <Route path={ROUTES.lang.call()} element={<BuyBitcoin />}>
             <Route index element={<Page />} />
-            <Route path={ROUTES.cart.products.path} element={<Products />} />
-            <Route path={ROUTES.blog.path} element={<BlogPost />} />
-            <Route path={ROUTES.cart.product.path} element={<ProductPage />} />
-            <Route path={ROUTES.about.path} element={<About />} />
-            <Route path={ROUTES.Support.path} element={<SupportPage />} />
-            <Route path={ROUTES.cart.path} element={<Cart />} />
-            <Route path={ROUTES.tutorials.path} element={<TutorialsPage />} />
-            <Route path={ROUTES.videos.path} element={<VideosPage />} />
-            <Route
-              path={ROUTES.paymentStatus.success.path}
-              element={<PaymentSuccess />}
-            />
-            <Route
-              path={ROUTES.policyPrivacy.path}
-              element={<PrivacyPolicy />}
-            />
           </Route>
           <Route path={ROUTES.lang.call()}>
-            <Route
-              path={ROUTES.paymentStatus.failure.path}
-              element={<PaymentFailure />}
-            />
             <Route path={ROUTES.cart.checkout.path} element={<Checkout />} />
             <Route path={ROUTES.buyBitcoin.path} element={<BuyBitcoin />} />
             <Route path={ROUTES.fee.path} element={<Fees />} />
             <Route path={ROUTES.buyCheckout.path} element={<BuyCheckout />} />
+            <Route path={ROUTES.Support.path} element={<SupportPage />} />
             <Route
               path={ROUTES.paymentAlfredStatus.success.path}
               element={<PaymentAlfredSuccess />}
@@ -146,14 +74,6 @@ export function BrowserRouter() {
               element={<AboutBuyBitcoin />}
             />
             <Route path={ROUTES.term.path} element={<TermsOfUse />} />
-            <Route
-              path={ROUTES.cart.pixPayment.path}
-              element={<PixPayment />}
-            />
-            <Route
-              path={ROUTES.cart.pixPayment.path}
-              element={<BitcoinPayment />}
-            />
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
