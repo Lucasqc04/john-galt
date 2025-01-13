2a3715168c8958b3298bd6713cbcc8811ec17fd9
jefflds <jeffersonlds3009@gmail.com>
2025-01-13 20:42:24 +0000

    refactor(main): comment gtm

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import * as TagManager from 'react-gtm-module';
+// import * as TagManager from 'react-gtm-module';
 import './domain/locales/Language';
 import { ThemeProvider } from './view/context/ThemeContext';
 import { App } from './view/screens/App';
 import './view/styles/index.css';
 
-const tagManagerArgs = {
-  gtmId: import.meta.env.VITE_GTM_ID,
-};
+// const tagManagerArgs = {
+//   gtmId: import.meta.env.VITE_GTM_ID,
+// };
 
-TagManager.initialize(tagManagerArgs);
+// TagManager.initialize(tagManagerArgs);
 
 const root = createRoot(document.getElementById('root')!);
 
