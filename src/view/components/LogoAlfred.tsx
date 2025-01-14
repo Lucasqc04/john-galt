2a5a6d3fbd4762b9b98c4cc68ec72e42c876a9b8
Portela03 <joao.portela03@gmail.com>
2025-01-14 13:22:39 +0000

    style: change font logo

--- src/view/components/LogoAlfred.tsx
+++ src/view/components/LogoAlfred.tsx
@@ -1,7 +1,9 @@
 export function LogoAlfred() {
   return (
     <div>
-      <h1 className="font-lexend font-bold dark:text-white">ALFRED</h1>
+      <h1 className="font-lexend text-[#F39200] dark:text-[#F39200] font-black text-5xl sm:text-6xl lg:text-7xl flex items-center ">
+        ALFRED
+      </h1>
     </div>
   );
 }
