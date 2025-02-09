c53947f174e753d725105a4b00f02a794b627ebe
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2025-02-09 23:58:26 +0000

    feat(checkout): add react-hook-forms

--- src/view/screens/Checkout/Checkout.tsx
+++ src/view/screens/Checkout/Checkout.tsx
@@ -1,122 +1,14 @@
-import { usecases } from '@/domain/usecases/UseCases';
 import AlfredWhiteLogo from '@/view/assets/logo/alfred-white-logo.svg';
 import SocialButtons from '@/view/components/SocialButtons';
-import { toZonedTime } from 'date-fns-tz';
-import { useEffect, useState } from 'react';
-import { FormProvider, useForm } from 'react-hook-form';
-import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import { FormProvider } from 'react-hook-form';
 import AlfredImg from '../../assets/_DIY SEC LAB - Apresentação Comercial (1).png';
 import Btc from '../../assets/bitcoin.svg';
 import Brl from '../../assets/brl.svg';
-import { ROUTES } from '../../routes/Routes';
-import { useCurrentLang } from '../../utils/useCurrentLang';
-
-export type GetCheckout = {
-  brlAmount: string;
-  btcAmount: string;
-};
+import { useCheckout } from './useCheckout';
 
 export default function Checkout() {
-  const navigate = useNavigate();
-  const { t } = useTranslation();
-  const { currentLang } = useCurrentLang();
-
-  const [btcRate, setBtcRate] = useState(0);
-  const [isTransactionAllowed, setIsTransactionAllowed] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await fetch(import.meta.env.VITE_API_URL);
-    };
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    const fetchBitcoinRate = async () => {
-      try {
-        const { result } = await usecases.bitcoinRate.list.execute();
-
-        if (result.type === 'ERROR') {
-          return;
-        }
-
-        setBtcRate(result.data.bitcoin.brl);
-        console.log(btcRate);
-      } catch (error) {
-        console.error('Erro ao buscar taxa de Bitcoin:', error);
-      }
-    };
-
-    fetchBitcoinRate();
-  }, [btcRate]);
-
-  useEffect(() => {
-    const timeZone = 'America/Sao_Paulo';
-    const now = new Date();
-    const zonedTime = toZonedTime(now, timeZone);
-    const currentHour = zonedTime.getHours();
-
-    if (currentHour < 8 || currentHour >= 22) {
-      setIsTransactionAllowed(false);
-    }
-  }, []);
-
-  const handleBrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.replace(/\D/g, '');
-    let numericValue = parseInt(value, 10);
-
-    if (isNaN(numericValue)) {
-      form.setValue('brlAmount', '');
-      form.setValue('btcAmount', '');
-      return;
-    }
-
-    if (numericValue > 5000) {
-      numericValue = 5000;
-    }
-
-    const formattedValue = new Intl.NumberFormat('pt-BR', {
-      style: 'currency',
-      currency: 'BRL',
-      minimumFractionDigits: 0,
-    }).format(numericValue);
-
-    form.setValue('brlAmount', formattedValue);
-
-    if (btcRate > 0 && numericValue >= 0) {
-      form.setValue('btcAmount', (numericValue / btcRate).toFixed(8));
-    }
-  };
-
-  async function onSubmit(data: GetCheckout) {
-    if (!isTransactionAllowed) {
-      toast.error(t('checkout.transaction_error'));
-      return;
-    }
-
-    const numericValue = parseInt(
-      form.getValues('brlAmount').replace(/\D/g, ''),
-      10,
-    );
-
-    if (numericValue >= 700 && numericValue <= 5000) {
-      localStorage.setItem('brlAmount', data.brlAmount);
-      localStorage.setItem('btcAmount', data.btcAmount);
-      navigate(ROUTES.buyCheckout.call(currentLang));
-    } else {
-      toast.warning(t('checkout.amount_error'));
-    }
-  }
-
-  const form = useForm<GetCheckout>({
-    mode: 'onChange',
-    defaultValues: {
-      brlAmount: '',
-      btcAmount: '',
-    },
-  });
+  const { form, handleBrlChange, onSubmit, t, isTransactionAllowed } =
+    useCheckout();
 
   return (
     <>
@@ -159,8 +51,8 @@ export default function Checkout() {
                 <div className="w-full flex justify-center items-center pt-4">
                   <div className="relative w-full">
                     <input
-                      readOnly
                       {...form.register('btcAmount', { required: true })}
+                      readOnly
                       placeholder={t('checkout.btc_placeholder')}
                       className="border-2 px-16 py-3 rounded-3xl text-base sm:text-lg text-white placeholder-white bg-transparent text-center w-full"
                     />
---
+++ src/view/screens/Checkout/useCheckout.ts
@@ -0,0 +1,123 @@
+import { usecases } from '@/domain/usecases/UseCases';
+import { ROUTES } from '@/view/routes/Routes';
+import { useCurrentLang } from '@/view/utils/useCurrentLang';
+import { toZonedTime } from 'date-fns-tz';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+export type GetCheckout = {
+  brlAmount: string;
+  btcAmount: string;
+};
+
+export function useCheckout() {
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+  const { currentLang } = useCurrentLang();
+
+  const [btcRate, setBtcRate] = useState(0);
+  const [isTransactionAllowed, setIsTransactionAllowed] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch(import.meta.env.VITE_API_URL);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchBitcoinRate = async () => {
+      try {
+        const { result } = await usecases.bitcoinRate.list.execute();
+
+        if (result.type === 'ERROR') {
+          return;
+        }
+
+        setBtcRate(result.data.bitcoin.brl);
+        console.log(btcRate);
+      } catch (error) {
+        console.error('Erro ao buscar taxa de Bitcoin:', error);
+      }
+    };
+
+    fetchBitcoinRate();
+  }, [btcRate]);
+
+  useEffect(() => {
+    const timeZone = 'America/Sao_Paulo';
+    const now = new Date();
+    const zonedTime = toZonedTime(now, timeZone);
+    const currentHour = zonedTime.getHours();
+
+    if (currentHour < 8 || currentHour >= 22) {
+      setIsTransactionAllowed(false);
+    }
+  }, []);
+
+  const handleBrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\D/g, '');
+    let numericValue = parseInt(value, 10);
+
+    if (isNaN(numericValue)) {
+      form.setValue('brlAmount', '');
+      form.setValue('btcAmount', '');
+      return;
+    }
+
+    if (numericValue > 5000) {
+      numericValue = 5000;
+    }
+
+    const formattedValue = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+      minimumFractionDigits: 0,
+    }).format(numericValue);
+
+    form.setValue('brlAmount', formattedValue);
+
+    if (btcRate > 0 && numericValue >= 0) {
+      form.setValue('btcAmount', (numericValue / btcRate).toFixed(8));
+    }
+  };
+
+  async function onSubmit(data: GetCheckout) {
+    if (!isTransactionAllowed) {
+      toast.error(t('checkout.transaction_error'));
+      return;
+    }
+
+    const numericValue = parseInt(
+      form.getValues('brlAmount').replace(/\D/g, ''),
+      10,
+    );
+
+    if (numericValue >= 700 && numericValue <= 5000) {
+      localStorage.setItem('brlAmount', data.brlAmount);
+      localStorage.setItem('btcAmount', data.btcAmount);
+      navigate(ROUTES.buyCheckout.call(currentLang));
+    } else {
+      toast.warning(t('checkout.amount_error'));
+    }
+  }
+
+  const form = useForm<GetCheckout>({
+    mode: 'onChange',
+    defaultValues: {
+      brlAmount: '',
+      btcAmount: '',
+    },
+  });
+
+  return {
+    t,
+    form,
+    handleBrlChange,
+    onSubmit,
+    isTransactionAllowed,
+  };
+}
