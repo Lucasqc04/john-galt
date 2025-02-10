e825ca5d64cc4f95a8548852dc7d2bc307e10b13
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2025-02-10 12:24:50 +0000

    feat: add bg-black in input

--- src/view/screens/Checkout/ValuesForm/ValuesForm.tsx
+++ src/view/screens/Checkout/ValuesForm/ValuesForm.tsx
@@ -13,7 +13,7 @@ export function ValuesForm() {
             {...form.register('brlAmount', { required: true })}
             onChange={handleBrlChange}
             placeholder={t('checkout.brl_placeholder')}
-            className="border-2 px-16 py-3 rounded-3xl text-base sm:text-lg text-white placeholder-white bg-transparent text-center w-full"
+            className="border-2 px-16 py-3 rounded-3xl text-base sm:text-lg text-white placeholder-white bg-black text-center w-full"
           />
           <button className="absolute right-2 top-1/2 -translate-y-1/2 text-white">
             <img
@@ -31,7 +31,7 @@ export function ValuesForm() {
             {...form.register('btcAmount', { required: true })}
             readOnly
             placeholder={t('checkout.btc_placeholder')}
-            className="border-2 px-16 py-3 rounded-3xl text-base sm:text-lg text-white placeholder-white bg-transparent text-center w-full"
+            className="border-2 px-16 py-3 rounded-3xl text-base sm:text-lg text-white placeholder-white bg-black text-center w-full"
           />
           <button className="absolute right-2 top-1/2 -translate-y-1/2 text-white">
             <img
